f07a44a62c3cd9453460e3a66804c3b398643ef9
dinesh <dinesh@activesphere.com>
2011-10-31 18:19:37 +0530

    changes in css and in code

--- jpicture_plugin/picture_carousel.js
+++ jpicture_plugin/picture_carousel.js
@@ -26,7 +26,7 @@ $(function() {
         plugin.set_image_size(image_width, image_height, current_obj);
         plugin.set_content_size(visible_image, image_width, content_height);
         plugin.set_image_grid_size(size, image_width, image_height);
-        plugin.set_scroll_number_in_footer(size, visible_image, scroll_width);
+        plugin.set_scroll_number(size, visible_image, scroll_width);
         $('#picture_content').append("<a href = '#' id = previous></a>");
         $('#picture_content').append("<a href='#' id = next></a>");
         plugin.setData('picture_content');
@@ -45,18 +45,19 @@ $(function() {
         if(current_id == 'next') {
           current_object.current_position = ++current_object.current_position;
           if(!(current_object.current_position <= image_position ) ) {
-            $("img").animate({"left": '-=' + image_width}, "slow");
+            $("ul.image_grid li").animate({"left": '-=' + image_width}, "slow");
             plugin.scroll_movement(current_object.current_position, size, visible_image, scroll_width, image_position);
           }
         } else if(current_id == 'previous') {
           current_object.current_position = --current_object.current_position;
           if(!((current_object.last_position >= ((size - (visible_image - image_position) ))) && (current_object.current_position>= ((size - (visible_image - image_position)))))) {
-            $("img").animate({"left": '+=' + image_width}, "slow");
+            $("ul.image_grid li").animate({"left": '+=' + image_width}, "slow");
             plugin.scroll_movement(current_object.current_position, size, visible_image, scroll_width, image_position);
           }
         } else {
           var current_position = $($obj).text();
           current_object.current_position = current_position;
+          console.log(current_position);
           if( !(current_position <= image_position && current_object.last_position <= image_position )) {
             plugin.scroll_movement(current_position, size, visible_image, scroll_width, image_position);
             plugin.image_movement(visible_image, size, current_position, image_width, image_position);            
@@ -68,10 +69,16 @@ $(function() {
       },
       set_image_size : function(img_width, img_height, object) {
         var name = $(object).attr('class');
-        $(' img').css({
-          'width': img_width, 
-          'height': img_height, 
+        $('.image_grid li').css({
+          'width': '360px',
+          'height': 'auto',
+          'float': 'left',
           'position': 'relative',
+          'list-style': 'none'
+        });
+        $(' img').css({
+          'width': img_width,
+          'height': img_height,
           'float': 'left'
         });
       },
@@ -90,18 +97,18 @@ $(function() {
           'height' : img_height
         });
       },
-      set_scroll_number_in_footer : function(size, visible, list_width) {
-        $('#picture_content').append('<div class="clear"></div>');
-        $('#picture_content').append('<div id = scroll></div>');
+      set_scroll_number : function(size, visible, list_width) {
+        // $('#picture_content').append('<div class="clear"></div>');
+        $('body').append('<div id = scroll></div>');
         var lt_width = parseInt(list_width, 10);
         for (var i = 1; i <= size; i++) {
-          $('#picture_content #scroll').append("<li><a href ='#' id = " + i + ">" + i + "</a></li>");
+          $('#scroll').append("<li><a href ='#' id = " + i + ">" + i + "</a></li>");
         }
         $('#scroll').css({'width': (size * (lt_width)) });
         $('#scroll li').css({'width': list_width});
         $('#scroll').append('<div id = active></div>');
         $('#active').css({'width': ((visible * (lt_width - 4))) + 'px'});
-        $('a').addClass('footer');
+        $('a').addClass('scroll_number');
       },
       hide_arrows : function(visible, size, width, image_position) {
         var current_object = $('#picture_content').data('jPicture_plugin');
@@ -135,7 +142,7 @@ $(function() {
             if (current_position === "" + ((parseInt(size,10) - i))) {
               if (current_object.first_click === 0) {
                 position = current_object.last_position == 0 ? (((parseInt(current_position, 10) -(parseInt(visible) - i)) - parseInt(current_object.last_position, 10) ) * parseInt(width, 10)) : plugin.image_move_at_last(current_position, image_position, size, visible, width);
-                $("img").animate({"left": '-=' + position + "px"}, "slow");
+                $("ul.image_grid li").animate({"left": '-=' + position + "px"}, "slow");
                 current_object.first_click++;
               }
               //by arrow it reach last hide the next arrow
@@ -146,7 +153,7 @@ $(function() {
         } else if((current_position < (size - (visible - image_position)) && current_object.last_position > (size - (visible - image_position))) && current_position > image_position) {
           current_object.first_click = 0;
           position = ((size - (visible - image_position)) - current_position) * parseInt(width, 10);
-          $("img").animate({"left": '+=' + position + "px"}, "slow");
+          $("ul.image_grid li").animate({"left": '+=' + position + "px"}, "slow");
         } else if(current_position < image_position && current_object.last_position > image_position ) {
           if (current_position < image_position && current_object.last_position > (size - (visible - image_position))) {
             position = ( (size - (visible - image_position)) - image_position) * parseInt(width, 10);
@@ -155,12 +162,12 @@ $(function() {
           }
           current_object.first_click = 0;
           current_object.last_position = current_position;
-          $("img").animate({"left": '+=' + position + "px"}, "slow");
+          $("ul.image_grid li").animate({"left": '+=' + position + "px"}, "slow");
         }
         else {
           current_object.first_click = 0
           position = ( ( parseInt (current_position, 10) - image_position) * parseInt(width, 10) );
-          $("img").animate({"left": -position + "px"}, "slow");
+          $("ul.image_grid li").animate({"left": -position + "px"}, "slow");
         }
         current_object.last_position = current_position;
       },
@@ -168,11 +175,12 @@ $(function() {
         var current_object = $('#picture_content').data('jPicture_plugin'), position;
         position = (parseInt(current_object.current_position, 10) - current_object.constant);
         $('#scroll a:eq('+ position +')').addClass('selected');
-        $('.image_grid img:eq('+ position +')').addClass('image_selected');
+        console.log('pos' + position);
+        console.log($('.image_grid li:eq('+ position + ')').addClass('image_selected'));
       },
       remove_previous_position : function() {
         $('#scroll a').removeClass('selected');
-        $('.image_grid img').removeClass('image_selected');
+        $('.image_grid li').removeClass('image_selected');
       },
       scroll_movement : function(current_position, size, visible, list_width, image_position) {
         var scroll_position = 0;
@@ -210,32 +218,26 @@ $(function() {
           current_object.first_click_scroll = 0;
           scroll_position = ( current_position - current_object.last_position) * lt_width;
         }
-        if (current_object.last_position < current_position && current_position % 5 == 0) {
-          if (current_position > 20 ) {
-            scroll_position += 5;
-          }
-          $('#active').css({'width': ((visible * (lt_width + 1)) + 5) + 'px'});
-        } else if (current_object.last_position > current_position && current_position % 5 == 0){
-          if (current_position < 20 ) {
-            scroll_position -= 5;
-          }
-        }
+        // if (current_object.last_position < current_position && current_position % 5 == 0) {
+        //           if (current_position > 20 ) {
+        //             scroll_position += 5;
+        //           }
+        //           $('#active').css({'width': ((visible * (lt_width + 1)) + 5) + 'px'});
+        //         } else if (current_object.last_position > current_position && current_position % 5 == 0){
+        //           if (current_position < 20 ) {
+        //             scroll_position -= 5;
+        //           }
+        //         }
         $("#scroll #active").animate({"right": '-=' + scroll_position + "px"}, "slow");
       }
     };
     $.fn.jPicture_plugin = function(options) {
       var width, image_width, current_obj;
       width = $('body').outerWidth();
-      if (options) {
-        image_width = Math.floor(width/options.image_visible);
-      } else {
-        image_width = Math.floor(width/5);
-      }
       var default_settings = {
-        image_width:  image_width +'px',
+        image_width: '360px',
         image_height: 'auto',
-        content_height: '340px',
-        list_width : '55px',
+        content_height: 'auto',
         image_position : 3,
         image_visible: 5
       };
@@ -246,7 +248,7 @@ $(function() {
         }
         current_obj = this;
         plugin.initialize_content(current_obj);
-        size = $('.image_grid img').size();
+        size = $('.image_grid li').size();
         default_settings.list_width = Math.floor(width/size);
         plugin.initialize_gird(default_settings.image_width, default_settings.image_height, current_obj, default_settings.image_visible, default_settings.content_height, size, default_settings.list_width);
         //disable the prevbutton initially if greater than the image_size
@@ -256,7 +258,7 @@ $(function() {
           $("#next").hide();
           $("#previous").hide();
         }
-        $("#picture_content a").live('click', function() {
+        $("body a").live('click', function() {
           plugin.image_process(default_settings.image_position, default_settings.image_width, default_settings.image_visible, default_settings.list_width, size, this);
         });
         plugin.show_current_position();        
